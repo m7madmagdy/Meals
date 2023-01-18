ce3afd84d606bf21f578b3847cd05b3fce50f773
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-01-18 03:52:46 +0200

    fixed data module and created data remote & repo

---
+++ data/src/main/java/com/data/repo/MealsRepoImp.kt
@@ -0,0 +1,9 @@
+package com.data.repo
+
+import com.data.remote.ApiService
+import com.domain.entity.CategoryResponse
+import com.domain.repo.MealsRepo
+
+class MealsRepoImp(private val apiService: ApiService) : MealsRepo {
+    override fun getMealsFromRemote(): CategoryResponse = apiService.getMeals()
+}
