02338329490b819c3c0a1ead46bc3801270ddb73
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-01-30 17:31:31 +0200

    Created Meals viewmodel

---
+++ app/src/main/java/com/meals/MealsViewModel.kt
@@ -0,0 +1,30 @@
+package com.meals
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.domain.entity.CategoryResponse
+import com.domain.usercase.GetMeals
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MealsViewModel @Inject constructor(private val getMealsUseCase: GetMeals) : ViewModel() {
+
+    private val _categories: MutableStateFlow<CategoryResponse?> = MutableStateFlow(null)
+    val categories: StateFlow<CategoryResponse?> = _categories
+
+    fun getMeals() {
+        viewModelScope.launch {
+            try {
+                _categories.value = getMealsUseCase()
+            } catch (e: Exception) {
+                Log.e("MealsViewModel", "getMeals: ${e.message}")
+            }
+        }
+    }
+
+}
